ff0d84a93eabac8172e75cb6e4b3ca0accc2ad74
Liza <lizaapostol378@gmail.com>
2024-10-03 17:50:27 +0300

    task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const images = [
   {
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
@@ -22,5 +24,12 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const html = images.map((image) => {
+  return `<li><img src="${image.url}" alt="${image.alt}"></li>`;
+});
+
+const list = document.querySelector(".gallery");
+list.insertAdjacentHTML("afterbegin", html.join(""));
