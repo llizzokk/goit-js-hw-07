0910b46e80ae4b220c6d9a8aabb5a35453c0e3cc
Liza <lizaapostol378@gmail.com>
2024-10-04 11:33:46 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,26 @@
+"use strict";
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const elements = event.currentTarget.elements;
+
+  if (
+    elements.email.value.length === 0 ||
+    elements.password.value.length === 0
+  ) {
+    alert("All form fields must be filled in");
+  } else {
+    const obj = {
+      email: elements.email.value.trim(),
+      password: elements.password.value.trim(),
+    };
+
+    console.log(obj);
+    event.target.reset();
+  }
+}
