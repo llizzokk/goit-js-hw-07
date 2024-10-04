1d506d1a9f5af105533d36f0177afa27f4c700bb
Liza <lizaapostol378@gmail.com>
2024-10-04 11:51:49 +0300

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,19 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+btn.addEventListener("click", handleClick);
+
+function handleClick() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
 }
